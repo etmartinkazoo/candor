805f2c2365782b551277c8ad5a1d18144afc37f5
Ted Martin <etmartinkazoo@gmail.com>
2019-04-07 20:09:24 -0400

    Page layout

--- full-width-page.php
+++ full-width-page.php
@@ -1,24 +1,27 @@
-<?php
+<?php /* Template Name: Sidebar */ ?>
 
-	get_header(); ?>
+<?php get_header(); ?>
 
-	<div class="container py-5">
-		<div class="row justify-content-center">
-			<div class="col-8">
-				<h2><?php the_title(); ?></h2>
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<div class="container py-5">
+	<div class="row">
+		<div class="col-md-8">
+			<h2><?php the_title(); ?></h2>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-					<?php the_content(); ?>
+				<?php the_content(); ?>
 
-					<?php if(comments_open() || get_comments_number()): ?>
-						<?php comments_template(); ?>
-					<?php endif; ?>
-
-				<?php endwhile; else : ?>
-					<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+				<?php if(comments_open() || get_comments_number()): ?>
+					<?php comments_template(); ?>
 				<?php endif; ?>
-			</div>
+
+			<?php endwhile; else : ?>
+				<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+			<?php endif; ?>
+		</div>
+		<div class="col-md-4">
+			<?php get_sidebar(); ?>
 		</div>
 	</div>
+</div>
 
-	<?php get_footer(); ?>
+<?php get_footer(); ?>
